e557829183e34e3ddb1e65152e2fd204d26a1daa
zeeshan.aslam <gujjarxzeeshan@gmail.com>
2024-12-06 19:17:52 +0500

    update ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,12 @@ import {
   MenuItem,
   SubMenu,
 } from "react-pro-sidebar";
-import { FaCartShopping, FaBars,FaChevronRight,FaChevronLeft } from "react-icons/fa6";
+import {
+  FaCartShopping,
+  FaBars,
+  FaChevronRight,
+  FaChevronLeft,
+} from "react-icons/fa6";
 import { RxDashboard } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import Logout from "../pages/logout";
@@ -29,8 +34,8 @@ function Sidebar() {
         <div className="p-6 bg-teal-700">
           <div className="flex justify-between items-center mb-10">
             <h2 className="text-3xl font-bold text-white flex items-center">
-              <FaCartShopping className="mr-2" />
-              {!collapsed && "Admin"}
+              <FaCartShopping classNa me="mr-2" />
+              {!collapsed && "admin"}
             </h2>
 
             <button
@@ -41,35 +46,33 @@ function Sidebar() {
             </button>
           </div>
 
-      
           <Menu className="font-medium pb-4">
             <MenuItem
               component={<Link to="/admin/dashboard" />}
               className="text-white hover:bg-teal-900 hover:text-teal-300 transition duration-200"
             >
               <div className="flex items-center">
-              <RxDashboard className="mr-2" />
-              {!collapsed && "Dashboard"}
+                <RxDashboard className="mr-2" />
+                {!collapsed && "Dashboard"}
               </div>
             </MenuItem>
             <MenuItem
               component={<Link to="/admin/product" />}
               className="text-white hover:bg-teal-900 hover:text-teal-300 transition duration-200"
             >
-           
-            Products
+              Products
             </MenuItem>
             <MenuItem
               component={<Link to="/admin/customer" />}
               className="text-white hover:bg-teal-900 hover:text-teal-300 transition duration-200"
             >
-             Customer
+              Customer
             </MenuItem>
             <MenuItem
               component={<Link to="/admin/order" />}
               className="text-white hover:bg-teal-900 hover:text-teal-300 transition duration-200"
             >
-             Order
+              Order
             </MenuItem>
           </Menu>
 
